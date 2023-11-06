48f0560ce8f6e9ddfdc1a681661fda115ac6b726
Whywehere <104337697+whywehere@users.noreply.github.com>
2023-11-06 13:10:12 +0800

    新增Slog 将日志保存至文件

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"flag"
+	"log/slog"
+	"os"
 	"shyIM/config"
+	"time"
 )
+
+var Slog *slog.Logger
 
 func main() {
 	var configPath string
@@ -12,4 +17,24 @@ func main() {
 	if err := config.Init(configPath); err != nil {
 		panic(err)
 	}
+}
+
+func init() {
+	file, err := os.OpenFile("./logfile.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+
+	Slog = slog.New(slog.NewJSONHandler(file, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     slog.LevelInfo,
+		ReplaceAttr: func(groups []string, attr slog.Attr) slog.Attr {
+			if attr.Key == slog.TimeKey {
+				if t, ok := attr.Value.Any().(time.Time); ok {
+					attr.Value = slog.StringValue(t.Format(time.DateTime))
+				}
+			}
+			return attr
+		},
+	}))
 }
